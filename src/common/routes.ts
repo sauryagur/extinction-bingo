7bb0bd4d2807fc3273cea6f81ae9dc7d804ba501
Saurya Gur <saurya.gur@outlook.com>
2025-10-14 16:50:23 +0530

    v1

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -3,9 +3,9 @@ import { Router } from 'express'
 const router: Router = Router()
 
 // import routes
-import userRouter from '../resources/users/routes'
+// import userRouter from '../resources/users/routes'
 
 // Higher level routes definition
-router.use('/user', userRouter)
+// router.use('/user', userRouter)
 
 export default router

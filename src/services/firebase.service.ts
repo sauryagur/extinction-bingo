8c048029ce6fc2fdb4b30f5768550488f4fe9dc2
Saurya Gur <saurya.gur@outlook.com>
2025-10-15 00:47:59 +0530

    refactor: firebase service is seperate from db service

---
+++ src/services/firebase.service.ts
@@ -0,0 +1,18 @@
+// src/services/firebase.service.ts
+import * as admin from 'firebase-admin'
+import { ServiceAccount } from 'firebase-admin'
+
+// Load your service account key securely from an environment variable.
+// Make sure this env var is set in your deployment environment!
+const serviceAccount: ServiceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  // If you were using the Realtime Database, you'd add:
+  // databaseURL: "https://extinction-bingo.firebaseio.com"
+})
+
+const db = admin.firestore() // Get the Firestore instance
+const auth = admin.auth() // Get the Authentication instance
+
+export { db, auth, admin } // Export these instances for use in other parts of your app
